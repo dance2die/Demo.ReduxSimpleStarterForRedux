f417805fcffe95e3b23b2d6dd904582f5c0ba45e
Sung <sungmkim80@gmail.com>
2017-05-29 19:12:21 -0400

    Updated ActionCreator(actions/index.js/selectBook) to return an action

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Javascript: the Good parts'},
+        {title: 'Harry POtter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'},
+    ];
+}
