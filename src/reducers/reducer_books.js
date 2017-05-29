7550ebe8397c35e828e8510532cf45a5e96f0180
Sung <sungmkim80@gmail.com>
2017-05-29 19:33:38 -0400

    Implemented BookDetail container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        {title: 'Javascript: the Good parts'},
-        {title: 'Harry POtter'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'},
+        {title: 'Javascript: the Good parts', pages: 101},
+        {title: 'Harry POtter', pages: 39},
+        {title: 'The Dark Tower', pages: 85},
+        {title: 'Eloquent Ruby', pages: 1},
     ];
 }
